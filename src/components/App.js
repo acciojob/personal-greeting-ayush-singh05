4143463aefdae698a3b7a85696776da405ed5bd9
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-12-19 15:51:37 +0000

    update 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {useStare} from 'react';
 const App = () => {
+
+  const [text,setText] = useState('');
+  
+  function handleClick(val){
+    setText("Hello " + val.target.value + "!");
+  }
   return (
     <div>
         {/* Do not remove the main div */}
