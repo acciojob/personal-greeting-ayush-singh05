40c2393a1eccf40f8ae707a6fb452499521c90e8
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-12-19 15:48:14 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>
+     <label htmlFor="name">Enter your name:</label>
+     </div>
+     <input htmlFor="name" type="text" onChange={handleClick} />
+
+     <p>{text}</p>
     </div>
   )
 }
