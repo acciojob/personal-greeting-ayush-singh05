af5a3ae4fda5ba97a34161f8a55cb2a08ce9d97a
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-12-19 16:02:52 +0000

    update 1.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import {useStare} from 'react';
+import React,{useStare} from 'react';
 const App = () => {
 
   const [text,setText] = useState('');
@@ -11,13 +11,10 @@ const App = () => {
   }
   return (
     <div>
-        {/* Do not remove the main div */}
-        <div>
-     <label htmlFor="name">Enter your name:</label>
-     </div>
-     <input htmlFor="name" type="text" onChange={handleClick} />
-
-     {!!text && (
+    {/* Do not remove the main div */}
+    <p>Enter your name:</p>
+    <input type="text" id="name" value={text} onChange={handleClick} />
+    {!!text && (
       <p>{`Hello ${text}!`}</p>
     )}
     </div>
